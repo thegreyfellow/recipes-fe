dcc80a0ca7fae2803c3fb70b00ce362fcff3c417
Youssef Idmoussi <youssef.idmoussi@gmail.com>
2021-07-29 12:51:48 +0100

    Update API URL and use env variables.

--- src/common/api.js
+++ src/common/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
+const baseUrl = process.env.REACT_APP_API_PATH || 'http://localhost:3001/api/'
+
 export default axios.create({
-  baseURL: 'http://localhost:3001/api/',
+  baseURL: baseUrl,
   headers: { 'Content-Type': 'application/json; charset=utf-8' },
 })
